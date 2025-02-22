9e1c2459e398d05c3251baeee2628c917ad55d7c
* <tbui17@users.noreply.github.com>
2025-02-22 15:17:38 -0700

    Add MyDynamicObject

---
+++ src/Shouldly.Tests/TestHelpers/MyDynamicObject.cs
@@ -0,0 +1,19 @@
+﻿namespace Shouldly.Tests.TestHelpers;
+
+class MyDynamicObject : DynamicObject
+{
+    private readonly Dictionary<string, object?> properties = new();
+
+    public override bool TryGetMember(GetMemberBinder binder, out object? result)
+    {
+        var key = binder.Name;
+        return properties.TryGetValue(key, out result);
+    }
+
+    public override bool TrySetMember(SetMemberBinder binder, object? value)
+    {
+        var key = binder.Name;
+        properties[key] = value;
+        return true;
+    }
+}
